1ef12888008d2ece9632cfe0e276aa6a1654b915
호이 <isouvezz@gmail.com>
2022-07-26 16:04:45 +0900

    [Network] 네트워크 Layer 구성 완료 (#3)
    * [#2] chore : 개발 초기 Setting
    
    * [#2] feat : Network DTO 및 Entity 구성
    
    * Update issue templates
    
    * Update issue templates
    
    * [#2] feat : Network Layer 구성

---
+++ Picterest/Network/Repository/PhotoInfoRepository.swift
@@ -0,0 +1,53 @@
+//
+//  PhotoInfoRepository.swift
+//  Picterest
+//
+//  Created by 장주명 on 2022/07/26.
+//
+
+import Foundation
+import UIKit
+
+class PhotoInfoRepository {
+    private let provider: Provider
+    private var imageCache = NSCache<NSURL,NSData>()
+    private var currentPage: Int = 0
+    
+    init(provider: Provider = ProviderImpl()) {
+        self.provider = provider
+    }
+    
+    func getPhotoList(completion: @escaping (Result<[PhotoInfo],Error>) -> Void) {
+        let photoListRequestDTO = PhotoInfoRequestDTO(page: currentPage)
+        let endpoint = APIEndpoints.getPhotosInfo(with: photoListRequestDTO)
+        currentPage += 1
+        provider.request(with: endpoint) { result in
+            switch result {
+            case .success(let responesDTO) :
+                var photoInfoList = [PhotoInfo]()
+                responesDTO.photoList.forEach { info in
+                    photoInfoList.append(info.toDomain())
+                }
+                completion(.success(photoInfoList))
+            case .failure(let error) :
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func loadImage(url: String, completion: @escaping (Result<Data,Error>) -> Void) {
+        if let url = NSURL(string: url), let data = imageCache.object(forKey: url) {
+            completion(.success(data as Data))
+        } else {
+            let endpoint = APIEndpoints.getImage(with: url)
+            provider.request(with: endpoint) { result in
+                switch result {
+                case .success(let data) :
+                    completion(.success(data))
+                case .failure(let error) :
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
